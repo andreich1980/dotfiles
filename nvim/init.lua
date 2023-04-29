85e02feaa12a892199aadb70ae492f3e4af91086
Andrew Ponomarev <spargo32@gmail.com>
2023-04-29 11:36:22 +0300

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_exec ('language en_US', true)
+
+require('user/plugins')
+require('user/options')
+require('user/keymaps')
+
---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Quit insert mode
+vim.keymap.set('i', 'jj', '<Esc>')
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+
+-- Reselect visual selection after indenting
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
+
+-- Maintain the cursor position when yanking a visual selection
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+vim.keymap.set('v', 'Y', 'myY`y')
+
+-- Paste replace visual selection without copying it
+vim.keymap.set('v', 'p', '"_dP')
+
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
+
+-- Move text up and down
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv")
+vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv")
+
