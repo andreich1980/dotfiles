bf2031e3739deddfc5ceaee8065985053037477a
Andrew Ponomarev <spargo32@gmail.com>
2026-05-11 20:38:32 +0300

    feat(02-01): implement vim module
    - back up ~/.vimrc
    - symlink ~/.vimrc_shared to repo's .vimrc
    - include source line in ~/.vimrc
    - symlink ~/.ideavimrc
    - create ~/.vim state directories (undo, backup, swap)

---
+++ modules/vim.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Vim configuration module
+
+echo "Configuring Vim..."
+
+VIMRC_SHARED="$HOME_DIR/.vimrc_shared"
+VIMRC_MAIN="$HOME_DIR/.vimrc"
+IDEAVIMRC="$HOME_DIR/.ideavimrc"
+
+# 1. Back up existing .vimrc if it exists and is not a symlink
+backup_file "$VIMRC_MAIN"
+
+# 2. Link our shared config
+safe_link "$DOTFILES_DIR/vim/.vimrc" "$VIMRC_SHARED"
+
+# 3. Include shared config in main .vimrc
+include_line "source ~/.vimrc_shared" "$VIMRC_MAIN"
+
+# 4. Link IdeaVim config
+safe_link "$DOTFILES_DIR/vim/.ideavimrc" "$IDEAVIMRC"
+
+# 5. Create state directories
+run mkdir -p "$HOME_DIR/.vim/undo" "$HOME_DIR/.vim/backup" "$HOME_DIR/.vim/swap"
